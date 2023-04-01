54ec0d5ea4e927bbf75061fb0728f8571fd1d685
Mateo Fileni <mateofileni@iMac-de-Mateo.local>
2023-04-01 10:53:37 -0300

    preEntrega1+Fileni

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './NavBar/NavBar.jsx';
+import ItemListContainer from './ItemListContainer/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
+        <ItemListContainer greeting={'Bienvenidos'} />
     </div>
     
   );
