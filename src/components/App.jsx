98e2b24b88ea254d23d86e4c88b3ffa5471235e7
Mateo Fileni <mateofileni@iMac-de-Mateo.local>
2023-04-19 15:02:28 -0300

    agrego ItemCount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './NavBar/NavBar.jsx';
 import ItemListContainer from './ItemListContainer/ItemListContainer';
+import ItemCount from './ItemCount/ItemCount.jsx';
 
 function App() {
   return (
     <div className="App">
         <NavBar />
         <ItemListContainer greeting={'Bienvenidos'} />
+        <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ',quantity)}/>
     </div>
     
   );
