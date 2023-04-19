568a7c3c51809cd5117e9a3523f315925ba31d84
Mateo Fileni <mateofileni@iMac-de-Mateo.local>
2023-04-19 18:45:57 -0300

    agrego ItemlistContanier, ItemList e Item

---
+++ src/asyncMock.js
@@ -0,0 +1,19 @@
+const products = [
+    {
+        id:'1',
+        name: 'Iphone 12',
+        price: 1000,
+        category: 'celular',
+        img:'https://cbafederal.net/w-content/uploads/2021/02/i-12-a.png',
+        stock: 25,
+        description: 'Descripcion de Iphone 12',
+    },
+]
+
+export const getProducts = () => {
+    return new Promise ((resolve) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 500)
+    })
+}
