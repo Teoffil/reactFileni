9ac54d7d3acb65bb9f5c0c63d9712218d8ceddb4
Mateo Fileni <mateofileni@iMac-de-Mateo.local>
2023-05-13 03:22:15 -0300

    consideraciones

--- src/services/firebase/firebaseConfig.js
+++ src/services/firebase/firebaseConfig.js
@@ -3,11 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    authDomain: "react-2023-fdf7c.firebaseapp.com",
-    projectId: "react-2023-fdf7c",
-    storageBucket: "react-2023-fdf7c.appspot.com",
-    messagingSenderId: "170264266366",
-    appId: "1:170264266366:web:66f06eabeab1e63bc3ba2f"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 
